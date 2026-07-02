c5cc31b653f1969006ad94ff7fdb3b5d736d3e82
uncor3 <contactuncor3@gmail.com>
2026-07-02 12:34:48 +0000

    update bridge header file

--- src/native/include/bridge.h
+++ src/native/include/bridge.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QImage>
+#include <cstddef>
 #include <cstdint>
 
 #ifdef __cplusplus
@@ -14,6 +15,8 @@ QImage generate_thumbnail_with_reader_ffi(const void *reader_ptr,
                                           int32_t file_size,
                                           int32_t requested_w,
                                           int32_t requested_h);
+
+QImage heic_to_image_ffi(const uint8_t *data, size_t len);
 
 #ifdef __cplusplus
 }
