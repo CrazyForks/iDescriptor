71654c155be923ba606ab1b3015c67c5551adedd
uncor3 <contactuncor3@gmail.com>
2026-07-03 08:40:36 +0000

    fix: update path to app icon in deploy script

--- scripts/deploy-appimage.sh
+++ scripts/deploy-appimage.sh
@@ -44,7 +44,7 @@ mkdir -p "$APPDIR/usr/share/icons/hicolor/256x256/apps"
 
 # Copy executable and icon
 cp target/release/idescriptor "$APPDIR/usr/bin/iDescriptor"
-cp resources/icons/app-icon/icon.png "$APPDIR/usr/share/icons/hicolor/256x256/apps/iDescriptor.png"
+cp packaging/shared/resources/app-icon/icon.png "$APPDIR/usr/share/icons/hicolor/256x256/apps/iDescriptor.png"
 
 
 # Copy ifuse
